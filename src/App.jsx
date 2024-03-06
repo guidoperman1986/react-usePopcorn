190f0a06df5ab29c54873ce24b5f9a2b8bd6afe2
guidopermanEleva <guido.perman@leniolabs.com>
2024-03-06 13:16:42 -0300

    rating stars added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { Main } from "./components/Main";
+import { ShowRating } from "./components/ShowRating";
 
 const tempMovieData = [
   {
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ShowRating } from "./components/ShowRating.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* <ShowRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={5}
+    />
+    <ShowRating maxRating={5} color={"crimson"} size={25} /> */}
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
